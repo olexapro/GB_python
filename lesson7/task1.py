95e962362564511f14351ca9b31cf54d251a153f
yohoko <paradox.wolf95@gmail.com>
2021-01-24 12:58:09 +0300

    1 task done

--- lesson7/task1.py
+++ lesson7/task1.py
@@ -29,39 +29,84 @@ class Matrix:
         return list1
 
     def __add__(self, other):
-        for i in range(len(self.matrix)):
-            if len(self.matrix[i]) > len(other.matrix[i]):
-                print('M1 > M2')
-                # если длинна матрицы 1 больше матрицы 2 то принимать значение первой как единственные
-                for j in range(len(other.matrix[i])):
-                    self.matrix[i][j] = self.matrix[i][j] + other.matrix[i][j]
+        if len(self.matrix) <= len(other.matrix):
+            print('M1 <= M2')
+            # если матрица1 меньше или равна матрице2 по количеству строк
 
-                for k in range(len(other.matrix[i]) - len(self.matrix[i]), 0, -1):
-                    self.matrix[i][k + len(other.matrix[i])] = self.matrix[i][k]
+            for i in range(len(self.matrix)):
+                if len(self.matrix[i]) > len(other.matrix[i]):
+                    # если длинна матрицы 1 больше матрицы 2 то принимать значение первой как единственные
+                    print('M1[i] > M2[i]')
+                    for j in range(len(other.matrix[i])):
+                        self.matrix[i][j] = self.matrix[i][j] + other.matrix[i][j]
 
-            else:
-                print('M1 <= M2')
-                # что бы не писать еще два ветвления я сделал так
-                for j in range(len(self.matrix[i])):
-                    self.matrix[i][j] = self.matrix[i][j] + other.matrix[i][j]
+                    for k in range(len(other.matrix[i]) - len(self.matrix[i]), 0, -1):
+                        self.matrix[i][k + len(other.matrix[i])] = self.matrix[i][k]
 
-                if len(self.matrix[i]) < len(other.matrix[i]):
-                    # если длинна матрицы 2 больше матрицы 1 то принимать значения второй как единственные
-                    print('M1 < M2')
-                    k = abs(len(self.matrix[i]) - len(other.matrix[i]))
-                    self.matrix[i] += other.matrix[i][-k:]
+                else:
+                    # что бы не писать еще два ветвления я сделал так
+                    print('M1[i] <= M2[i]')
+                    for j in range(len(self.matrix[i])):
+                        self.matrix[i][j] = self.matrix[i][j] + other.matrix[i][j]
 
-        return self.matrix
+                    if len(self.matrix[i]) < len(other.matrix[i]):
+                        # если длинна матрицы 2 больше матрицы 1 то принимать значения второй как единственные
+                        print('M1[i] < M2[i]')
+                        k = abs(len(self.matrix[i]) - len(other.matrix[i]))
+                        self.matrix[i] += other.matrix[i][-k:]
+
+            if len(self.matrix) < len(other.matrix):
+                print('M1 < M2')
+                self.matrix += other.matrix[-1:]
+
+        else:
+            # если матрица1 больше матрицы2 по количеству строк
+            print('M1 > M2')
+
+            for i in range(len(self.matrix) - len(self.matrix) - len(other.matrix)):
+                if len(self.matrix[i]) > len(other.matrix[i]):
+                    # если длинна матрицы 1 больше матрицы 2 то принимать значение первой как единственные
+                    print('M1[i] > M2[i]')
+                    for j in range(len(other.matrix[i])):
+                        self.matrix[i][j] = self.matrix[i][j] + other.matrix[i][j]
+
+                    for k in range(len(other.matrix[i]) - len(self.matrix[i]), 0, -1):
+                        self.matrix[i][k + len(other.matrix[i])] = self.matrix[i][k]
+
+                else:
+                    # что бы не писать еще два ветвления я сделал так
+                    print('M1[i] <= M2[i]')
+                    for j in range(len(self.matrix[i])):
+                        self.matrix[i][j] = self.matrix[i][j] + other.matrix[i][j]
+
+                    if len(self.matrix[i]) < len(other.matrix[i]):
+                        # если длинна матрицы 2 больше матрицы 1 то принимать значения второй как единственные
+                        print('M1[i] < M2[i]')
+                        k = abs(len(self.matrix[i]) - len(other.matrix[i]))
+                        self.matrix[i] += other.matrix[i][-k:]
+
+        return Matrix(self.matrix)
+
+    def generate(self, y, x, nums=9):
+        return Matrix([[randint(0, nums) for _ in range(x)] for _ in range(y)])
 
 
-matrix1 = Matrix([[randint(0, 9) for _ in range(6)] for _ in range(2)])
-print(matrix1)
+diap_i = 3
+diap_j = 5
+for i in range(1, diap_i):
+    for j in range(1, diap_j):
+        m1 = Matrix().generate(i, j)
+        m2 = Matrix().generate(i, j)
+        print(f'M1: {m1}\nM2: {m2}\nm21: {m2 + m1}')
 
-matrix2 = Matrix([[randint(0, 9) for _ in range(5)] for _ in range(2)])
-print(matrix2)
+for i in range(1, diap_i):
+    for j in range(1, diap_j):
+        m1 = Matrix().generate(i, j)
+        m2 = Matrix().generate(i + 3, j + 3)
+        print(f'M1: {m1}\nM2: {m2}\nm21: {m2 + m1}')
 
-m21 = Matrix(matrix1 + matrix2)
-print(m21)
-
-# for i in matrix1:
-#     print(i)
+for i in range(1, diap_i):
+    for j in range(1, diap_j):
+        m1 = Matrix().generate(i + 3, j + 3)
+        m2 = Matrix().generate(i, j)
+        print(f'M1: {m1}\nM2: {m2}\nm21: {m2 + m1}')
